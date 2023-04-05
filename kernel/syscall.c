d1eae457bd625bab9bbd114efde25dfec98ac2f8
tmerc10 <thomasdmercurio@gmail.com>
2023-04-05 23:04:51 +0100

    changing nanotime

--- kernel/syscall.c
+++ kernel/syscall.c
@@ -85,7 +85,7 @@ sys_nanotime(void)
 {
     uint64 nanotime;
     //printf("%d\n", *
-    nanotime = *(KERNBASE + VIRTMEM) | *(KERNBASE + VIRTMEM + 4);
+    nanotime = *(uint64*)(KERNBASE + VIRTMEM) | *(uint64*)(KERNBASE + VIRTMEM + 4);
     nanotime = 0;
     return nanotime;
 }
